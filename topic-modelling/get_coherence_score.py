a7114947c6028169ccc45b2c9ca647459178a779
Ott Martens <ott.martens@pipedrive.com>
2020-04-13 21:30:42 +0300

    Coherence calculations

--- topic-modelling/get_coherence_score.py
+++ topic-modelling/get_coherence_score.py
@@ -4,7 +4,8 @@ from logging import log, INFO
 
 import gensim
 
-from utils import readCSV
+from utils import *
+from preprocess_helpers import createDictionary
 
 logging.basicConfig(
     format='%(asctime)s : %(levelname)s : %(message)s', level=INFO)
@@ -12,26 +13,42 @@ logging.basicConfig(
 
 def main():
     try:
-        model_name = sys.argv[1]
+        corpus_path = sys.argv[1]
+        model_names = sys.argv[2:]
     except:
-        print('specify name of the model from the command line')
+        print('specify corpus file and model name(s) from the command line')
         exit(1)
-
-    model_path = 'models/{}'.format(model_name)
-    corpus_path = 'models/{}_lemmatized.csv'.format(model_name)
+    
+    corpus_path = 'models/{}'.format(corpus_path)
 
     log(INFO, 'Parsing texts from file')
     _, texts = readCSV(corpus_path)
 
-    log(INFO, 'Loading topic model from file')
-    model = gensim.utils.SaveLoad.load(model_path)
+    # Generate a dictionary of the corpus
+    log(INFO, "Generating a dictionary")
+    id2word = createDictionary(texts)
 
-    log(INFO, 'Generating a cohenrence model')
-    coherence_model = gensim.models.CoherenceModel(model, texts=texts)
+    coherences = []
 
-    coherence = coherence_model.get_coherence()
+    for model_name in model_names:
 
-    print('Coherence of {} : {}'.format(model_name, coherence))
+        log(INFO, "Loading model from models/{}".format(model_name))
+        model_path = 'models/{}'.format(model_name)
+
+        log(INFO, 'Loading topic model from file')
+        model = gensim.utils.SaveLoad.load(model_path)
+
+        log(INFO, 'Generating a cohenrence model')
+        coherence_model = gensim.models.CoherenceModel(model, texts=texts)
+
+        coherence = coherence_model.get_coherence()
+        coherences.append(coherence)
+    
+    for model_name, coherence in list(zip(model_names, coherences)):
+        log(INFO, "Coherence of {}: {}".format(model_name, coherence))
+    
+    writeCSV("coherences.csv", list(zip(model_names, coherences)))
+    log(INFO, "Finished")
 
 
 if __name__ == '__main__':
